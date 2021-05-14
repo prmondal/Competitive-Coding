0e96346b7a6c087eda4d7909b4b8f7e88871cb17
Prasenjit Mondal <prasenjit.mondal@autodesk.com>
2021-05-14 11:17:30 +0530

    used gilbertorder

--- snippets/mos.h
+++ snippets/mos.h
@@ -1,20 +1,42 @@
 int BLOCK_SIZE;
 
+inline ll gilbertOrder(int x, int y, int pow, int rotate) {
+	if (pow == 0) {
+		return 0;
+	}
+	int hpow = 1 << (pow-1);
+	int seg = (x < hpow) ? (
+		(y < hpow) ? 0 : 3
+	) : (
+		(y < hpow) ? 1 : 2
+	);
+	seg = (seg + rotate) & 3;
+	const int rotateDelta[4] = {3, 0, 0, 1};
+	int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
+	int nrot = (rotate + rotateDelta[seg]) & 3;
+	ll subSquareSize = 1LL << (2*pow - 2);
+	ll ans = seg * subSquareSize;
+	ll add = gilbertOrder(nx, ny, pow-1, nrot);
+	ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
+	return ans;
+}
+
 struct Query {
     int l;
     int r;
     int idx;
     int block;
+    ll ord;
 
     Query() {}
-    
+
     Query(int l, int r, int idx): l(l), r(r), idx(idx) {
         block = l / BLOCK_SIZE;
+        ord = gilbertOrder(l, r, 21, 0);
     }
 
     bool operator<(const Query& o) const {
-        if (block != o.block) return block < o.block;
-        return (block & 1) ? r < o.r : r > o.r;
+        return ord < o.ord;
     }
 };
 
